d1f3c8d42bbe7ab193390da4fe78431b59ce2045
Yanjaa Wintersoul <ywintersoul@theclimateservice.com>
2022-06-05 07:39:52 -0700

    some wording

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Contract from "./pages/Contract";
+import { useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { channels } from "@epnsproject/frontend-sdk-staging";
+import "./App.scss";
+
+const BASE_URL = "https://backend-kovan.epns.io/apis";
+const CHANNEL_ADDRESS = "0x94c3016ef3e503774630fC71F59B8Da9f7D470B7";
 
 function App() {
+  const { library, active, account, chainId } = useWeb3React();
+
+  // create state components to fetch all the notifications.
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  // channel details
+  const [channel, setChannel] = useState(null);
+  // load channel details on start
+  useEffect(() => {
+    if (!account) return;
+    // on page load, fetch channel details
+    channels.getChannelByAddress(CHANNEL_ADDRESS, BASE_URL).then((data) => {
+      setChannel(data);
+    });
+    // fetch if user is subscribed to channel
+    channels.isUserSubscribed(account, CHANNEL_ADDRESS).then((res) => {
+      console.log(res);
+      setIsSubscribed(res);
+    });
+  }, [account]);
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +39,55 @@ function App() {
         <Route path="/category" element={<Category />}></Route>
         <Route path="/contract" element={<Contract />}></Route>
       </Routes>
+      <div className="App">
+      {/* define the header */}
+      <h2 className="App__header">
+        <span> EPNS Playground </span>
+      </h2>
+      {/* define the header */}
+
+      {active ? (
+        <>
+          {/* section for channels */}
+          {channel && (
+            <div>
+              <h3>Sample clear
+                code Channel</h3>
+              <div className="sample__channel">
+                <div>
+                  <img src={channel.icon} className="channel__image" />
+                  <h2>{channel.name}</h2>
+                </div>
+                <div
+                  onClick={() => {
+                    isSubscribed
+                      ? channels.optOut(
+                          library.getSigner(account),
+                          channel.addr,
+                          chainId,
+                          account,
+                        )
+                      : channels.optIn(
+                          library.getSigner(account),
+                          channel.addr,
+                          chainId,
+                          account,
+                        );
+                  }}
+                  className="subscribebutton"
+                >
+                  {isSubscribed ? "unsubscribe" : "subscribe"}
+                </div>
+              </div>
+            </div>
+          )}
+          {/* section for channels */}
+
+        </>
+      ) : (
+        <p>Please connect to your wallet on the kovan test network to proceed</p>
+      )}
+    </div>
     </BrowserRouter>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,15 +22,13 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 py-16">
         <div className="text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Data to enrich your</span>{" "}
+            <span className="block xl:inline">Indexing the world's 🌎 most poppin' data to</span>{" "}
             <span className="block text-indigo-600 xl:inline">
-              online business
+            accelerate your journey to crypto success.
             </span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+            What's trending for whales, influencers, wallets - find out what the cool kids are doing. All categorized for ease-of-use.
           </p>
         </div>
         {/* <div className="mt-8 md:mt-16">

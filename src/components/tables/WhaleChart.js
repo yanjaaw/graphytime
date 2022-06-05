b9e7968d630aa9a2432e4be4f36b8f20dfb1c03e
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-06-04 19:45:24 -0700

    make chart component

---
+++ src/components/tables/WhaleChart.js
@@ -0,0 +1,69 @@
+export default function WhaleChart({ title, whales }) {
+  return (
+    <div className="mb-32">
+      <h2 className="text-xl font-semibold text-gray-900">{title} Whales</h2>
+      <p className="mt-2 text-sm text-gray-700">Copy defining whales</p>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+                  >
+                    Rank
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Address
+                  </th>
+
+                  <th
+                    scope="col"
+                    className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
+                  >
+                    No. NFTs
+                  </th>
+                  <th
+                    scope="col"
+                    className="relative py-3.5 pl-3 pr-4 sm:pr-6 md:pr-0"
+                  >
+                    <span className="sr-only">View</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {whales.map((whale, index) => (
+                  <tr key={whale.email}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 md:pl-0">
+                      {index + 1}
+                    </td>
+                    <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
+                      {whale.title}
+                    </td>
+                    <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
+                      {whale.role}
+                    </td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 md:pr-0">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        View
+                        <span className="sr-only">, {whale.name}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,5 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import Layout from "../components/Layout";
+import WhaleChart from "../components/tables/WhaleChart";
 
 export default function Category() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -22,74 +23,10 @@ export default function Category() {
         <h1 className="text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl mb-4">
           Analysis on {formattedCategoryName} NFT Collections
         </h1>
-        <p className="text-base text-gray-700">Copy about analyzing NFTs</p>
-        <div className="mt-16 mb-32">
-          <h2 className="text-xl font-semibold text-gray-900">
-            {formattedCategoryName} NFT Whales
-          </h2>
-          <p className="mt-2 text-sm text-gray-700">Copy defining whales</p>
-          <div className="mt-8 flex flex-col">
-            <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
-                      >
-                        Rank
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Address
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900"
-                      >
-                        No. NFTs
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3.5 pl-3 pr-4 sm:pr-6 md:pr-0"
-                      >
-                        <span className="sr-only">View</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200">
-                    {people.map((person, index) => (
-                      <tr key={person.email}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 md:pl-0">
-                          {index + 1}
-                        </td>
-                        <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
-                          {person.title}
-                        </td>
-                        <td className="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
-                          {person.role}
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 md:pr-0">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            View
-                            <span className="sr-only">, {person.name}</span>
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p className="text-base text-gray-700 mb-16">
+          Copy about analyzing NFTs
+        </p>
+        <WhaleChart title={`${formattedCategoryName} NFT`} whales={people} />
         <div className="mb-32">
           <h2 className="text-xl font-semibold text-gray-900">
             {formattedCategoryName} NFT Influencers
